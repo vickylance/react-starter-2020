a282b05a795cfed0bc9167238cd2758fb1659b17
Vignesh S <vignesh6993@live.in>
2020-11-30 18:18:20 +0530

    Initial react config

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    "no-descending-specificity": null,
+  },
+};
---
+++ config/webpack.config.js
@@ -0,0 +1,162 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const PrettierPlugin = require("prettier-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+// const StylelintPlugin = require("stylelint-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const DashboardPlugin = require("webpack-dashboard/plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+const outputPath = path.resolve(__dirname, "../dist");
+
+const eslintOptions = {
+  emitWarning: true,
+  failOnError: true,
+  failOnWarning: true,
+  fix: true,
+  outputReport: true,
+};
+
+const prettierConfig = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailing: true,
+  encoding: "utf-8",
+  extensions: [".js", ".jsx"],
+};
+
+const config = {
+  mode: isDevelopment ? "development" : "production",
+  entry: ["./src/index.js"],
+  output: {
+    path: outputPath,
+    filename: "[name].[contenthash].js",
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: outputPath,
+    compress: true,
+    hot: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    // alias: {
+    //   "react-dom": "@hot-loader/react-dom",
+    // },
+  },
+  plugins: [
+    new DashboardPlugin(),
+    new CleanWebpackPlugin(),
+    new PrettierPlugin(prettierConfig),
+    // new StylelintPlugin(),
+    new ESLintPlugin(eslintOptions),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false,
+    }),
+    new MiniCssExtractPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        extractComments: true,
+      }),
+    ],
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.hot) {
+    // Cannot use 'contenthash' when hot reloading is enabled.
+    config.output.filename = "[name].[hash].js";
+  }
+
+  return config;
+};
